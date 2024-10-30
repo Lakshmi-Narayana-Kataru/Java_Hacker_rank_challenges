fe1da84d6c37c453d9766bee4b92da7cfcccc71b
KATARU LAKSHMI NARAYANA <lakshman.k2934@gmail.com>
2024-10-30 09:26:58 +0530

    Add files via upload

---
+++ Hacker_Rank_02_Read_and_Print.java
@@ -0,0 +1,23 @@
+package com.learning.hacker_rank;
+
+import java.io.*;
+import java.util.*;
+
+public class Hacker_Rank_02_Read_and_Print {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+         int b= sc.nextInt();
+          int c= sc.nextInt();
+          System.out.println(a);
+          System.out.println(b);
+          System.out.println(c);
+          
+        
+
+
+
+
+    }
+}
---
+++ Hacker_Rank_03_If_Condition.java
@@ -0,0 +1,10 @@
+package com.learning.hacker_rank;
+
+public class Hacker_Rank_03_If_Condition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
